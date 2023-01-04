c3fd4bbf1f668193c8efbbf8ab1cce6e3646fa1b
STINKpython <121897714+STINKpython@users.noreply.github.com>
2023-01-03 21:33:48 -0300

    Update soldier_rifle.py

--- soldier_rifle.py
+++ soldier_rifle.py
@@ -6,8 +6,8 @@ class Soldier_rifle:
     def __init__(self,x,y):
 
         # Animaciones
-        self.run = Auxiliar.getSurfaceFromSpriteSheet("imegenes/soldier/run and point.png",8,1,False,1,1)[:6]
-        self.atack =  Auxiliar.getSurfaceFromSpriteSheet("imegenes/soldier/run and point.png",8,1,False,1,1)[6:8]       
+        self.run = Auxiliar.getSurfaceFromSpriteSheet("unidades/soldier/run and point.png",8,1,False,1,1)[:6]
+        self.atack =  Auxiliar.getSurfaceFromSpriteSheet("unidades/soldier/run and point.png",8,1,False,1,1)[6:8]       
         self.frame = 0
         self.animation = self.run
         self.image = self.animation[self.frame]
@@ -28,22 +28,17 @@ class Soldier_rifle:
     def move_y(self):
         self.rect.y += 0
 
-    def actions(self,delta_ms,lista_eventos,enemigo):
+    def actions(self,delta_ms,lista_eventos):
         
-        if self.rect_radar.colliderect(enemigo):
-            self.animation=self.atack
-            
-        else:
-            
-            self.animation=self.run
-            self.move_x()
+        self.animation=self.run
+        self.move_x()
 
 
-    def update(self,delta_ms,lista_eventos,enemigo):
+    def update(self,delta_ms,lista_eventos):
         self.tiempo_transcurrido = 0
 
         if self.live:
-            self.actions(delta_ms,lista_eventos,enemigo)
+            self.actions(delta_ms,lista_eventos)
             self.tiempo_transcurrido += delta_ms
         
             if self.tiempo_transcurrido > self.frame_rate_ms:  
